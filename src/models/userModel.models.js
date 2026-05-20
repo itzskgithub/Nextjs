6aa047f3c1d018e952aad16588939be8830b3efb
itzskgithub <shubhampandey8969@gmail.com>
2026-05-20 20:06:34 +0530

    created user model

---
+++ src/models/userModel.models.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Please enter username"],
+        unique: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: [true, "Please enter email"],
+        unique: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Please enter password"]
+    },
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpiry: Date,
+})
+
+const User = mongoose.models.users || mongoose.model("User", userSchema);
+
+export default User;
