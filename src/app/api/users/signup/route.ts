c1ce9bb0315569d81b722634121bc371b01e3aab
itzskgithub <shubhampandey8969@gmail.com>
2026-05-20 21:59:31 +0530

    Wrote code for signup in backend

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,52 @@
+import {connect} from "@/dbConfig/dbConfig";
+import bcryptjs from "bcryptjs";
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/userModel.models";
+
+connect();
+
+export async function POST(request: NextRequest){
+    try {
+        const reqBody = await request.json();
+        const {username, email, password} = reqBody;
+
+        //little validation 
+        if(!username || !email || !password){
+            return NextResponse.json({
+                error: "All fields are required",
+                status: 400
+            });
+        }
+
+        const user = await User.findOne({email});
+        if(user){
+            return NextResponse.json(
+                {error: "User already exists"},
+                {status: 400}
+            )
+        }
+
+        //Hash password
+
+        const salt = await bcryptjs.genSalt(10);
+        const hashedPassword = await bcryptjs.hash(password, salt);
+
+        const newUser = new User({
+            username,
+            email,
+            password: hashedPassword
+        });
+
+        const savedUser = await newUser.save();
+        console.log(savedUser);
+        return NextResponse.json({
+            message: "User created successfully",
+            success: true,
+            savedUser
+        })
+
+    } catch (error: any) {
+        return NextResponse.json({error: error.message}, {status: 500});
+    }
+
+}
