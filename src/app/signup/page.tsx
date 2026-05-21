71c704b0fdd170356152c587baf4b56850fb94b3
itzskgithub <shubhampandey8969@gmail.com>
2026-05-21 11:34:40 +0530

    connect front end and backend to get and post date for signup

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -20,7 +20,9 @@ export default function SignupPage() {
 
     const onSignup = async () => {
         try {
-            console.log(user);
+            const response = await axios.post("/api/users/signup", user)
+            console.log("Signup success", response.data);
+            router.push("/login");
         } catch (error:any) {
             console.log("Signup failed", error.message);
             toast.error(error.message);
